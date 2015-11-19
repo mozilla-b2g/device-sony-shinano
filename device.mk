1c3cee1d3b1c6348d29d64b870ca4e0821714391
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2015-11-19 12:31:46 +0100

    Bug 1226158 - Update Sony blobs to v13 r=mwu

--- device.mk
+++ device.mk
@@ -243,4 +243,4 @@ PRODUCT_COPY_FILES += \
     device/sony/shinano/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
     device/sony/shinano/volume.cfg:system/etc/volume.cfg \
     hardware/sony/timekeep/gecko/TimeKeepService.js:system/b2g/distribution/bundles/timekeep/TimeKeepService.js \
-    hardware/sony/timekeep/gecko/chrome.manifest:system/b2g/distribution/bundles/timekeep/chrome.manifest
+    hardware/sony/timekeep/gecko/chrome.manifest:system/b2g/distribution/bundles/timekeep/chrome.manifest \
